a928e850df68da9328a2fa66441952abdcb48b39
Pieter Claerhout <pieter@yellowduck.be>
2020-05-28 17:51:15 +0200

    go-james 1.6.0 release

--- go-james.rb
+++ go-james.rb
@@ -1,9 +1,9 @@
 class GoJames < Formula
     desc "James is your butler and helps you to create, build, debug, test and run your Go projects"
     homepage "https://github.com/pieterclaerhout/go-james"
-    url "https://github.com/pieterclaerhout/go-james/releases/download/v1.5.2/go-james_darwin_amd64.tar.gz"
-    sha256 "93dd08bf97d1369bd0497c01b2d014511036a728f0b2f9da61f7f3e03a2683de"
-    version "1.5.2"
+    url "https://github.com/pieterclaerhout/go-james/releases/download/v1.6.0/go-james_darwin_amd64.tar.gz"
+    sha256 "39ca0dcacf5f11415a6a529ed97f11ee7e06c8c681813fd1b44d498f313682a1"
+    version "1.6.0"
   
     bottle :unneeded
   
