cc4b534f18fe428883ce56cc006fd4fde12fb9b2
Pieter Claerhout <pieter@yellowduck.be>
2021-10-26 14:00:57 +0200

    Fixed the SHA key

--- go-james.rb
+++ go-james.rb
@@ -2,7 +2,7 @@ class GoJames < Formula
     desc "James is your butler and helps you to create, build, debug, test and run your Go projects"
     homepage "https://github.com/pieterclaerhout/go-james"
     url "https://github.com/pieterclaerhout/go-james/releases/download/v1.8.1/go-james_darwin_amd64.tar.gz"
-    sha256 "642252a89b7813e4af849dcbe41fed60a846ca88cf4eb795a6873c7c6b0d53ac%"
+    sha256 "642252a89b7813e4af849dcbe41fed60a846ca88cf4eb795a6873c7c6b0d53ac"
     version "1.8.1"
 
     bottle :unneeded
