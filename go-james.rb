504abe7c0350e1cc7da6c0b17e5050405e5299ca
Pieter Claerhout <pieter@yellowduck.be>
2021-10-26 13:57:53 +0200

    Updated for v1.8.1

--- go-james.rb
+++ go-james.rb
@@ -1,12 +1,12 @@
 class GoJames < Formula
     desc "James is your butler and helps you to create, build, debug, test and run your Go projects"
     homepage "https://github.com/pieterclaerhout/go-james"
-    url "https://github.com/pieterclaerhout/go-james/releases/download/v1.6.0/go-james_darwin_amd64.tar.gz"
-    sha256 "39ca0dcacf5f11415a6a529ed97f11ee7e06c8c681813fd1b44d498f313682a1"
-    version "1.6.0"
-  
+    url "https://github.com/pieterclaerhout/go-james/releases/download/v1.8.1/go-james_darwin_amd64.tar.gz"
+    sha256 "642252a89b7813e4af849dcbe41fed60a846ca88cf4eb795a6873c7c6b0d53ac%"
+    version "1.8.1"
+
     bottle :unneeded
-  
+
     def install
       bin.install "go-james"
     end
